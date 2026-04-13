e0e38c34861c69bb9838770605c7f4d8a1a99264
Md Abu Farah <confusedengineer.md@gmail.com>
2026-04-14 00:15:04 +0530

    Ch-5 Operators Done

---
+++ Ch-5 Operators/arithmetic.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+// Arithmetic Operators
+// +, -, *, /, %
+// ++, --
+// +=, -=, *=, /=
+
+int main() {
+
+    // Initialization
+  int A = 10; 
+  int B = 20;
+
+  int add = A + B;// A = 10, B = 20
+  printf("A + B = %d\n", add); 
+
+  int substract = A - B;
+  printf("A - B = %d\n", substract);
+
+  int multiplies = A * B;
+  printf("A x B = %d\n", multiplies);
+
+  int divide = A / B;
+  printf("A / B = %d\n", divide);
+
+  int modulus = A % B;
+  printf("A modulus B = %d\n", modulus);
+
+  int increment = A++;
+  printf("A increment = %d\n", increment);
+
+  int decrement = A--;
+  printf("A decrement = %d\n", decrement);
+}
---
+++ Ch-5 Operators/assignment.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+// Assignment Operators
+// =, +=, -=, *=, /=, %=, <<=, >>=, &=, ^=, |=
+
+int main() {
+  int a = 21;
+  int c;
+
+  c = a;
+  printf("with = operator, value of c = %d\n", c);
+
+  c += a; // equivalent to c = c + a
+  printf("with += operator, value of c = %d\n", c);
+
+  c -= a; // equivalent to c = c - a
+  printf("with -= operator, value of c = %d\n", c);
+
+  c *= a; // equivalent to c = c x a
+  printf("with *= operator, value of c = %d\n", c);
+
+  c /= a; // equivalent to c = c / a
+  printf("with /= operator, value of c = %d\n", c);
+
+  c = 200;
+  c %= a; // equivalent to c = c % a
+  printf("with modulus= operator, value of c = %d\n", c);
+
+  c <<= a; // equivalent to c = c << a
+  printf("with <<= operator, value of c = %d\n", c);
+
+  c >>= a; // equivalent to c = c >> a
+  printf("with >>= operator, value of c = %d\n", c);
+
+  c &= a; // equivalent to c = c & a
+  printf("with &= operator, value of c = %d\n", c);
+  
+  c ^= a; // equivalent to c = c ^ a
+  printf("with ^= operator, value of c = %d\n", c);
+
+  c |= a; // equivalent to c = c | a
+  printf("with |= operator, value of c = %d\n", c);
+}
